c0652493b1e5fc980fbaceebe90a462e4b80f85c
J.A. Iguacel <infoiguacel@gmail.com>
2020-04-17 14:33:06 +0200

    margin not vh

--- src/exp/1/Ascensor.js
+++ src/exp/1/Ascensor.js
@@ -6,7 +6,7 @@ function Ascensor({ data }) {
     <div
       style={{
         width: "100%",
-        height: "100vh",
+        height: "900px",
         maxWidth: "900px",
         margin: "0 auto",
         border: "1px solid gray",
--- src/exp/1/WayPoint.js
+++ src/exp/1/WayPoint.js
@@ -8,10 +8,9 @@ export default function ({ onEnter, data }) {
       style={{
         position: "relative",
         width: "100%",
-        height: "100vh",
+        height: "900px",
         maxWidth: "900px",
         margin: "0 auto",
-        border: "1px solid gray",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
