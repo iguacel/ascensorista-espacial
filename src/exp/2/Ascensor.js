238e23e7a41518e2d3c095d2762865a24a92db0d
J.A. Iguacel <infoiguacel@gmail.com>
2020-04-18 12:33:07 +0200

    third

--- src/exp/2/Ascensor.js
+++ src/exp/2/Ascensor.js
@@ -5,9 +5,9 @@ function Ascensor({ data, windowHeight }) {
   return (
     <div
       style={{
-        width: "100%",
         height: `${windowHeight}px`,
-        maxWidth: "900px",
+        width: "100%",
+        maxWidth: "600px",
         margin: "0 auto",
         display: "flex",
         justifyContent: "center",
@@ -15,12 +15,13 @@ function Ascensor({ data, windowHeight }) {
         position: isSafari ? "-webkit-sticky" : "sticky",
         top: 0,
         zIndex: 2,
+        border: "1px solid gold"
       }}
     >
       <div className="ascensor" style={{ position: "relative" }}>
-        <h4 className="skewed">
+        {/* <h4 className="skewed">
           <span>{4 - data.index}</span>
-        </h4>
+        </h4> */}
         <img
           src={`./img/2/ascensor/${data.index}.png`}
           alt=""
--- src/exp/2/WayPoint.js
+++ src/exp/2/WayPoint.js
@@ -3,9 +3,7 @@ import { Waypoint } from "react-waypoint";
 
 export default function ({ onEnter, data, windowHeight }) {
   return (
-
     <div
-      className="final"
       style={{
         position: "relative",
         width: "100%",
@@ -15,11 +13,12 @@ export default function ({ onEnter, data, windowHeight }) {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        background: `url(./img/2/bg/${data.index}.png)`,
+        border: "1px solid green",
+        // background: `url(./img/2/bg/${data.index}.png)`,
         backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
+        // backgroundRepeat: "no-repeat",
         backgroundPosition: "center",
-        border: "1px solid pink"
+        // border: "1px solid pink"
       }}
     >
       <Waypoint
@@ -27,54 +26,67 @@ export default function ({ onEnter, data, windowHeight }) {
         onEnter={() => onEnter(data.index)}
         topOffset="50%"
       />
-      {/* <p
-        style={{
-          position: "relative",
-          zIndex: 3,
-          margin: "0 60px 0 60px",
-          background: "RGBA(255, 255, 255, 0.90)",
-          padding: "1em",
-          border: "1px solid var(--foreground-color)",
-        }}
-      >
-        {data.texto}
-      </p> */}
-
-
-      {/* Foreground */}
-
       <div
-        className="final"
         style={{
           position: "relative",
           width: "100%",
           height: `${windowHeight}px`,
-          maxWidth: "900px",
+          maxWidth: "600px",
           margin: "0 auto",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-          background: `url(./img/2/fg/${data.index}.png)`,
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "center",
-          border: "1px solid pink",
-          zIndex: 3
+          border: "1px solid red",
+          zIndex: "2",
         }}
       >
-        <Waypoint
-          onPositionChange={() => data && data.index && data.index}
-          onEnter={() => onEnter(data.index)}
-          topOffset="50%"
-        />
-
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            right: 0,
+            width: "100%",
+            height: `${windowHeight}px`,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            border: "1px solid red",
+            zIndex: "2",
+          }}
+        >
+          <img
+            src={`./img/2/fg/${data.index}.png`}
+            alt=""
+            style={{ width: "100%", top: "0", left: "0" }}
+          />
+        </div>
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            right: 0,
+            width: "100%",
+            height: `${windowHeight}px`,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            border: "1px solid red",
+            zIndex: "2",
+          }}
+        >
+          <p
+            style={{
+              zIndex: 3,
+              margin: "0 60px 0 60px",
+              background: "RGBA(255, 255, 255, 0.90)",
+              padding: "1em",
+              border: "1px solid var(--foreground-color)",
+            }}
+          >
+            {data.texto}
+          </p>
+        </div>
       </div>
     </div>
-
-
-
-
   );
 }
-
-
