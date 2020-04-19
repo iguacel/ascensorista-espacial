521776674612a6520e10f1c66edea1e042d8007d
J.A. Iguacel <infoiguacel@gmail.com>
2020-04-19 16:43:15 +0200

    zoom

--- src/components/main/Footer.js
+++ src/components/main/Footer.js
@@ -15,15 +15,27 @@ export default () => {
           alignItems: "center",
         }}
       >
-        <h6 style={{
-          fontWeight: 400, letterSpacing: "0.05em", fontSize: "0.8em",
-        }}>
+        <h6
+          style={{
+            fontWeight: 400,
+            letterSpacing: "0.05em",
+            fontSize: "0.7em",
+            textTransform: "uppercase",
+            lineHeight: "1.6em",
+          }}
+        >
           THE PILOT DOG
         </h6>
 
-        <h6 style={{
-          fontWeight: 400, letterSpacing: "0.05em", fontSize: "0.8em",
-        }}>
+        <h6
+          style={{
+            fontWeight: 400,
+            letterSpacing: "0.05em",
+            fontSize: "0.7em",
+            textTransform: "uppercase",
+            lineHeight: "1.6em",
+          }}
+        >
           <a
             href="https://calvichis.com/"
             target="_blank"
