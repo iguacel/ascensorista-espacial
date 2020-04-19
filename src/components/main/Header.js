954d5f47b888cf227e804c24c256aec2e05b7749
J.A. Iguacel <infoiguacel@gmail.com>
2020-04-19 16:03:33 +0200

    header

--- src/components/main/Header.js
+++ src/components/main/Header.js
@@ -30,10 +30,30 @@ export default () => {
           </div>
         </div>
         <div
-          className="sharer"
-          style={{ fontSize: "0.8em", width: "120px", marginLeft: "auto", paddingTop: ".5em", background: "gold" }}
+          className="cabecera"
+          style={{
+            fontSize: "0.8em",
+            marginLeft: "auto",
+            paddingTop: "1.5em",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
         >
-          face - twitter - whatsapp - telegram
+          <div className="sharer">
+            <h6
+              style={{
+                fontWeight: 400,
+                letterSpacing: "0.05em",
+                fontSize: "0.75em",
+                textTransform: "uppercase",
+                lineHeight: "1.6em",
+              }}
+            >
+              Webcomics de <strong>Calvichis</strong>, <strong>The Pilot Dog</strong> y otros artistas
+            </h6>{" "}
+          </div>
         </div>
       </header>
     </>
