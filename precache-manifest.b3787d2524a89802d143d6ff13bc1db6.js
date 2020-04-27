6027694744050bdd5eb56c40abe12118110bbfb4
J.A. Iguacel <infoiguacel@gmail.com>
2020-04-27 19:18:18 +0200

    Updates

---
+++ precache-manifest.b3787d2524a89802d143d6ff13bc1db6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fe98a9214a98fab5b8e5a7e6ecbb42f",
+    "url": "/ascensorista-espacial/index.html"
+  },
+  {
+    "revision": "957241a5b754f6c039a0",
+    "url": "/ascensorista-espacial/static/css/4.a051f53f.chunk.css"
+  },
+  {
+    "revision": "0509dd8f55c45b9b7297",
+    "url": "/ascensorista-espacial/static/css/5.c195c81b.chunk.css"
+  },
+  {
+    "revision": "ff1ef09ab93c6dffbb90",
+    "url": "/ascensorista-espacial/static/css/main.8b25e0b8.chunk.css"
+  },
+  {
+    "revision": "8340ebe7ee5765b5079a",
+    "url": "/ascensorista-espacial/static/js/2.9fe27f6d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ascensorista-espacial/static/js/2.9fe27f6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85cb528ed6e418357a1d",
+    "url": "/ascensorista-espacial/static/js/3.789f53a7.chunk.js"
+  },
+  {
+    "revision": "957241a5b754f6c039a0",
+    "url": "/ascensorista-espacial/static/js/4.961d9004.chunk.js"
+  },
+  {
+    "revision": "0509dd8f55c45b9b7297",
+    "url": "/ascensorista-espacial/static/js/5.70fe8db2.chunk.js"
+  },
+  {
+    "revision": "b71aff31d1748c474631",
+    "url": "/ascensorista-espacial/static/js/6.23a4702e.chunk.js"
+  },
+  {
+    "revision": "11632a58e510552bf3ae8fe3d05874e6",
+    "url": "/ascensorista-espacial/static/js/6.23a4702e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff1ef09ab93c6dffbb90",
+    "url": "/ascensorista-espacial/static/js/main.a418e1dd.chunk.js"
+  },
+  {
+    "revision": "c81660c76c74077dfee2",
+    "url": "/ascensorista-espacial/static/js/runtime-main.d1159b67.js"
+  },
+  {
+    "revision": "b3d490aaafc0687821b93fccb6af7ead",
+    "url": "/ascensorista-espacial/static/media/burguer.b3d490aa.mp3"
+  },
+  {
+    "revision": "efdadc4fdd6f838a12461f7c37b1a2f2",
+    "url": "/ascensorista-espacial/static/media/click.efdadc4f.mp3"
+  },
+  {
+    "revision": "b3d490aaafc0687821b93fccb6af7ead",
+    "url": "/ascensorista-espacial/static/media/info.b3d490aa.mp3"
+  },
+  {
+    "revision": "45cb7e95613299662fbca71136092b35",
+    "url": "/ascensorista-espacial/static/media/laffayette_comic_pro-webfont.45cb7e95.woff2"
+  },
+  {
+    "revision": "c161315de7c0a1a12295903706ab44fb",
+    "url": "/ascensorista-espacial/static/media/laffayette_comic_pro-webfont.c161315d.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ascensorista-espacial/precache-manifest.8532491f0f12b601f9dabf2a46b812f5.js"
+  "/ascensorista-espacial/precache-manifest.b3787d2524a89802d143d6ff13bc1db6.js"
 );
 
 self.addEventListener('message', (event) => {
