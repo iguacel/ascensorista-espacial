f8301596b36a35e6c651c327fd7c12321f1478fa
J.A. Iguacel <infoiguacel@gmail.com>
2020-06-29 13:15:01 +0200

    add3

--- src/exp/index.js
+++ src/exp/index.js
@@ -64,7 +64,7 @@ export default {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Francisco Asencio
+          Francisco Asencio.
         </a>.
       </p>)
   },
